f3c89a3107b7cd0e76f888410c12ebf26a7d7800
dwichan0905 <dwi.permana@aksaramaya.com>
2022-08-01 12:10:56 +0700

    Using columns

--- 1. Basic/app/src/main/java/idn/dwichan/jetpackcomposebasic/MainActivity.kt
+++ 1. Basic/app/src/main/java/idn/dwichan/jetpackcomposebasic/MainActivity.kt
@@ -3,6 +3,7 @@ package idn.dwichan.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -28,10 +29,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello, $name!",
-            modifier = Modifier.padding(24.dp)
-        )
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello, ")
+            Text(text = "$name!")
+        }
     }
 }
 
