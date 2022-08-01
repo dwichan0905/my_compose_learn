13e4f0c5c2416756f38f0633a4e40f6d42bbbcbe
dwichan0905 <dwi.permana@aksaramaya.com>
2022-08-01 13:24:24 +0700

    State hoisting

--- 1. Basic/app/src/main/java/idn/dwichan/jetpackcomposebasic/MainActivity.kt
+++ 1. Basic/app/src/main/java/idn/dwichan/jetpackcomposebasic/MainActivity.kt
@@ -3,18 +3,24 @@ package idn.dwichan.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,6 +33,36 @@ class MainActivity : ComponentActivity() {
             JetpackComposeBasicTheme {
                 // A surface container using the 'background' color from the theme
                 MyApp()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "App Preview", widthDp = 320)
+@Composable
+fun AppPreview() {
+    JetpackComposeBasicTheme {
+        MyApp()
+    }
+}
+
+@Preview(showBackground = true, name = "Onboarding Screen")
+@Composable
+fun OnboardingScreen(onContinue: () -> Unit = {}) {
+    Surface {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to Basic Codelab!")
+            Button(
+                modifier = Modifier
+                    .padding(24.dp),
+                onClick = onContinue
+            ) {
+                Text(text = "Continue")
             }
         }
     }
@@ -72,19 +108,21 @@ fun Greeting(name: String) {
 
 @Composable
 private fun MyApp(names: List<String> = listOf("Esmeralda", "Hylos")) {
-    Surface(color = MaterialTheme.colorScheme.background) {
-        Column {
-            for (name in names) {
-                Greeting(name = name)
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(
+            onContinue = {
+                shouldShowOnboarding = false
+            }
+        )
+    } else {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Column {
+                for (name in names) {
+                    Greeting(name = name)
+                }
             }
         }
-    }
-}
-
-@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeBasicTheme {
-        MyApp()
     }
 }
