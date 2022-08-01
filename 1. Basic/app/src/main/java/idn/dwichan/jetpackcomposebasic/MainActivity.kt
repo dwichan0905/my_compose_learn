c53950b9592099b0d9bc3d01b56c0db9af502a9a
dwichan0905 <dwi.permana@aksaramaya.com>
2022-08-01 12:04:24 +0700

    Recompose

--- 1. Basic/app/src/main/java/idn/dwichan/jetpackcomposebasic/MainActivity.kt
+++ 1. Basic/app/src/main/java/idn/dwichan/jetpackcomposebasic/MainActivity.kt
@@ -3,13 +3,14 @@ package idn.dwichan.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import idn.dwichan.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeBasicTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
@@ -31,13 +27,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello, $name!",
+            modifier = Modifier.padding(24.dp)
+        )
+    }
+}
+
+@Composable
+private fun MyApp() {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Greeting(name = "Dwi Candra Permana")
+    }
 }
 
 @Preview(showBackground = true, name = "Text Preview")
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
