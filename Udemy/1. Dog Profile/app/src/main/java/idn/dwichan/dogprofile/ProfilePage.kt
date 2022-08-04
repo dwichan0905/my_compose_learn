14b26b474672dc149aa51748d5b53f62b205279f
dwichan0905 <dwi.permana@aksaramaya.com>
2022-08-04 13:42:27 +0700

    Unused imports

--- Udemy/1. Dog Profile/app/src/main/java/idn/dwichan/dogprofile/ProfilePage.kt
+++ Udemy/1. Dog Profile/app/src/main/java/idn/dwichan/dogprofile/ProfilePage.kt
@@ -11,11 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
