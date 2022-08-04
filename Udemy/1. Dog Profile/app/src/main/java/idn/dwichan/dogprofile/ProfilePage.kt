414a6fdffbd6b2d733e13e7d298a44f610dda935
dwichan0905 <dwi.permana@aksaramaya.com>
2022-08-05 01:40:39 +0700

    Decoupling constraint with ConstraintSet - Applicable to dual orientation (portrait and landscape orientation)

--- Udemy/1. Dog Profile/app/src/main/java/idn/dwichan/dogprofile/ProfilePage.kt
+++ Udemy/1. Dog Profile/app/src/main/java/idn/dwichan/dogprofile/ProfilePage.kt
@@ -3,6 +3,7 @@ package idn.dwichan.dogprofile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,13 +20,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
+
+private const val LAYOUT_ID_IMAGE = "image"
+private const val LAYOUT_ID_NAME = "name"
+private const val LAYOUT_ID_COUNTRY = "country"
+private const val LAYOUT_ID_PROFILE_STATS = "profileStats"
+private const val LAYOUT_ID_FOLLOW_BUTTON = "followButton"
+private const val LAYOUT_ID_MESSAGE_BUTTON = "messageButton"
 
 @Composable
 fun ProfilePage() {
@@ -39,99 +51,66 @@ fun ProfilePage() {
                 end = 16.dp
             )
     ) {
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            // ConstraintLayout IDs
-            val (
-                image,
-                name,
-                country,
-                profileStats,
-                followButton,
-                messageButton
-            ) = createRefs()
-
-            val guideline = createGuidelineFromTop(fraction = 0.25F)
-
-            Image(
-                painter = painterResource(id = R.drawable.husky),
-                contentDescription = "Husky",
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-                    .border(
-                        width = 2.dp,
-                        color = Color.Red,
-                        shape = CircleShape
-                    )
-                    .constrainAs(image) {
-                        top.linkTo(guideline)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    },
-                contentScale = ContentScale.Crop
-            )
-            Text(
-                text = "Siberian Husky",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .constrainAs(name) {
-                        top.linkTo(image.bottom)
-                        start.linkTo(image.start)
-                        end.linkTo(image.end)
-                    }
-            )
-            Text(
-                text = "Germany",
-                modifier = Modifier
-                    .constrainAs(country) {
-                        top.linkTo(name.bottom)
-                        start.linkTo(name.start)
-                        end.linkTo(name.end)
-                    }
-            )
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-                    .constrainAs(profileStats) {
-                        top.linkTo(country.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }
-            ) {
-                ProfileStats(count = 150, title = "Followers")
-                ProfileStats(count = 120, title = "Following")
-                ProfileStats(count = 10, title = "Posts")
+        BoxWithConstraints {
+            // orientation config
+            val constraints = if (maxWidth < 600.dp) {
+                portraitConstraintSet(margin = 16.dp)
+            } else {
+                landscapeConstraintSet(margin = 16.dp)
             }
 
-            Button(
-                onClick = {  },
+            ConstraintLayout(
+                constraintSet = constraints,
                 modifier = Modifier
-                    .constrainAs(followButton) {
-                        top.linkTo(profileStats.bottom, margin = 16.dp)
-                        start.linkTo(parent.start)
-                        end.linkTo(messageButton.start)
-                        width = Dimension.wrapContent
-                    }
+                    .fillMaxSize()
             ) {
-                Text(text = "Follow user")
-            }
-            Button(
-                onClick = {  },
-                modifier = Modifier
-                    .constrainAs(messageButton) {
-                        top.linkTo(profileStats.bottom, margin = 16.dp)
-                        start.linkTo(followButton.end)
-                        end.linkTo(parent.end)
-                        width = Dimension.wrapContent
-                    }
-            ) {
-                Text(text = "Message")
+                Image(
+                    painter = painterResource(id = R.drawable.husky),
+                    contentDescription = "Husky",
+                    modifier = Modifier
+                        .layoutId(LAYOUT_ID_IMAGE)
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .border(
+                            width = 2.dp,
+                            color = Color.Red,
+                            shape = CircleShape
+                        ),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Siberian Husky",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.layoutId(LAYOUT_ID_NAME)
+                )
+                Text(
+                    text = "Germany",
+                    modifier = Modifier.layoutId(LAYOUT_ID_COUNTRY)
+                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                        .layoutId(LAYOUT_ID_PROFILE_STATS)
+                        .fillMaxWidth()
+                ) {
+                    ProfileStats(count = 150, title = "Followers")
+                    ProfileStats(count = 120, title = "Following")
+                    ProfileStats(count = 10, title = "Posts")
+                }
+
+                Button(
+                    onClick = {  },
+                    modifier = Modifier.layoutId(LAYOUT_ID_FOLLOW_BUTTON)
+                ) {
+                    Text(text = "Follow user")
+                }
+                Button(
+                    onClick = {  },
+                    modifier = Modifier.layoutId(LAYOUT_ID_MESSAGE_BUTTON)
+                ) {
+                    Text(text = "Message")
+                }
             }
         }
     }
@@ -145,6 +124,102 @@ fun ProfileStats(count: Int, title: String) {
     }
 }
 
+// portrait layout
+private fun portraitConstraintSet(margin: Dp): ConstraintSet =
+    ConstraintSet {
+        val image = createRefFor(id = LAYOUT_ID_IMAGE)
+        val name = createRefFor(id = LAYOUT_ID_NAME)
+        val country = createRefFor(id = LAYOUT_ID_COUNTRY)
+        val profileStats = createRefFor(id = LAYOUT_ID_PROFILE_STATS)
+        val followButton = createRefFor(id = LAYOUT_ID_FOLLOW_BUTTON)
+        val messageButton = createRefFor(id = LAYOUT_ID_MESSAGE_BUTTON)
+
+        val guideline = createGuidelineFromTop(fraction = 0.25F)
+
+        constrain(image) {
+            top.linkTo(guideline)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(name) {
+            top.linkTo(image.bottom)
+            start.linkTo(image.start)
+            end.linkTo(image.end)
+        }
+
+        constrain(country) {
+            top.linkTo(name.bottom)
+            start.linkTo(name.start)
+            end.linkTo(name.end)
+        }
+
+        constrain(profileStats) {
+            top.linkTo(country.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(followButton) {
+            top.linkTo(profileStats.bottom, margin = margin)
+            start.linkTo(parent.start)
+            end.linkTo(messageButton.start)
+            width = Dimension.wrapContent
+        }
+
+        constrain(messageButton) {
+            top.linkTo(profileStats.bottom, margin = margin)
+            start.linkTo(followButton.end)
+            end.linkTo(parent.end)
+            width = Dimension.wrapContent
+        }
+    }
+
+private fun landscapeConstraintSet(margin: Dp): ConstraintSet =
+    ConstraintSet {
+        val image = createRefFor(id = LAYOUT_ID_IMAGE)
+        val name = createRefFor(id = LAYOUT_ID_NAME)
+        val country = createRefFor(id = LAYOUT_ID_COUNTRY)
+        val profileStats = createRefFor(id = LAYOUT_ID_PROFILE_STATS)
+        val followButton = createRefFor(id = LAYOUT_ID_FOLLOW_BUTTON)
+        val messageButton = createRefFor(id = LAYOUT_ID_MESSAGE_BUTTON)
+
+        constrain(image) {
+            top.linkTo(parent.top, margin = margin)
+            start.linkTo(parent.start, margin = margin)
+        }
+
+        constrain(name) {
+            top.linkTo(image.bottom, margin = margin)
+            start.linkTo(parent.start, margin = margin)
+        }
+
+        constrain(country) {
+            top.linkTo(name.bottom)
+            start.linkTo(parent.start, margin = margin)
+        }
+
+        constrain(profileStats) {
+            top.linkTo(image.top, margin = margin)
+            start.linkTo(image.end, margin = 16.dp)
+            end.linkTo(parent.end)
+        }
+
+        constrain(followButton) {
+            top.linkTo(profileStats.bottom)
+            start.linkTo(parent.start, margin = 16.dp)
+            end.linkTo(messageButton.start)
+            width = Dimension.wrapContent
+        }
+
+        constrain(messageButton) {
+            top.linkTo(profileStats.bottom)
+            start.linkTo(followButton.end, margin = 16.dp)
+            end.linkTo(parent.end)
+            width = Dimension.wrapContent
+        }
+    }
+
 @Preview(
     showBackground = true,
     name = "Profile Page Preview",
@@ -152,5 +227,16 @@ fun ProfileStats(count: Int, title: String) {
 )
 @Composable
 fun ProfilePagePreview() {
+    ProfilePage()
+}
+
+@Preview(
+    showBackground = true,
+    name = "Profile Page Preview - Landscape",
+    showSystemUi = true,
+    device = Devices.TABLET
+)
+@Composable
+fun ProfilePagePreviewLandscape() {
     ProfilePage()
 }
