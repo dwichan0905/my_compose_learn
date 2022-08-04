af196fe4f3ea7bf88615ff0bf4087b81beeef906
dwichan0905 <dwi.permana@aksaramaya.com>
2022-08-04 13:40:53 +0700

    Card & Vertical Scroll

--- Udemy/1. Dog Profile/app/src/main/java/idn/dwichan/dogprofile/ProfilePage.kt
+++ Udemy/1. Dog Profile/app/src/main/java/idn/dwichan/dogprofile/ProfilePage.kt
@@ -9,8 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,46 +30,60 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfilePage() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                top = 100.dp,
+                bottom = 100.dp,
+                start = 16.dp,
+                end = 16.dp
+            )
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.husky),
-            contentDescription = "Husky",
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-                .border(
-                    width = 2.dp,
-                    color = Color.Red,
-                    shape = CircleShape
-                ),
-            contentScale = ContentScale.Crop
-        )
-        Text(text = "Siberian Husky")
-        Text(text = "Germany")
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
-            ProfileStats(count = 150, title = "Followers")
-            ProfileStats(count = 120, title = "Following")
-            ProfileStats(count = 10, title = "Posts")
-        }
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Button(onClick = {  }) {
-                Text(text = "Follow user")
+            Image(
+                painter = painterResource(id = R.drawable.husky),
+                contentDescription = "Husky",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .border(
+                        width = 2.dp,
+                        color = Color.Red,
+                        shape = CircleShape
+                    ),
+                contentScale = ContentScale.Crop
+            )
+            Text(text = "Siberian Husky")
+            Text(text = "Germany")
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ProfileStats(count = 150, title = "Followers")
+                ProfileStats(count = 120, title = "Following")
+                ProfileStats(count = 10, title = "Posts")
             }
-            Button(onClick = {  }) {
-                Text(text = "Message")
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Button(onClick = {  }) {
+                    Text(text = "Follow user")
+                }
+                Button(onClick = {  }) {
+                    Text(text = "Message")
+                }
             }
         }
     }
@@ -80,7 +99,8 @@ fun ProfileStats(count: Int, title: String) {
 
 @Preview(
     showBackground = true,
-    name = "Profile Page Preview"
+    name = "Profile Page Preview",
+    showSystemUi = true
 )
 @Composable
 fun ProfilePagePreview() {
