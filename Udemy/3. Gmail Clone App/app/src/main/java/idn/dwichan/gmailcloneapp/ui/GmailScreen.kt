9b5fca1af28a848b00cdfdaedb6e63d63b2e1133
dwichan0905 <dwi.permana@aksaramaya.com>
2022-08-07 02:03:23 +0700

    Add Scaffold for Layouting, and AppBar

--- Udemy/3. Gmail Clone App/app/src/main/java/idn/dwichan/gmailcloneapp/ui/GmailScreen.kt
+++ Udemy/3. Gmail Clone App/app/src/main/java/idn/dwichan/gmailcloneapp/ui/GmailScreen.kt
@@ -1,12 +1,23 @@
 package idn.dwichan.gmailcloneapp.ui
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import idn.dwichan.gmailcloneapp.ui.component.HomeAppBar
 import idn.dwichan.gmailcloneapp.ui.theme.GmailCloneAppTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GmailScreen() {
+    // default Material3 UI Layout such as topAppBar, botomAppBar, etc...
+    Scaffold(
+        topBar = { HomeAppBar() }
+    ) {
 
+    }
 }
 
 @Preview(
